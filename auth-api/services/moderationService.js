21c107af4f6cc33362873bd868afeddcda43c430
GURU.01 <vedantzalke14@gmail.com>
2025-03-15 12:14:07 +0100

    changes in backend

--- auth-api/services/moderationService.js
+++
@@ -1,8 +0,0 @@
-const Filter = require('bad-words');
-
-function checkExplicitContent(text) {
-    const filter = new Filter();
-    return filter.isProfane(text); // Returns true if explicit content
-}
-
-module.exports = { checkExplicitContent };
